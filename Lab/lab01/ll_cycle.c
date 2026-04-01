d3146e84be54fc40207f8227b3b513566266639b
BinElo-Xu <3488173930@qq.com>
2026-04-01 17:26:09 +0800

    Finishing all the lab01 contents.

--- Lab/lab01/ll_cycle.c
+++ Lab/lab01/ll_cycle.c
@@ -3,5 +3,24 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *tortoise = head, *hare = head;
+    if (!hare) {
+	return 0;
+    }
+
+    if (!hare->next) {
+	return 0;
+    }
+
+    while (hare->next) {
+	if (!hare->next->next) {
+	    return 0;
+	}
+	hare = hare->next->next;
+	tortoise = tortoise->next;
+	if (hare == tortoise) {
+	    return 1;
+	}
+    }
     return 0;
 }
